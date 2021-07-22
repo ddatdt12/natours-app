b8a0957afb8237f77d1b3e89fdf119bff4ab0aa8
Thanh Dat <ddatdt12@gmail.com>
2021-07-22 20:52:35 +0700

    Fb login n

--- public/js/fbLogin.js
+++ public/js/fbLogin.js
@@ -33,6 +33,8 @@ function testAPI() {
   // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
   console.log('Welcome!  Fetching your information.... ');
   FB.api('/me', function(response) {
+    console.log(response);
+    avatar.setAttribute('src', response.picture.data.url);
     console.log('Successful login for: ' + response.name);
     document.getElementById('status').innerHTML =
       'Thanks for logging in, ' + response.name + '!';
